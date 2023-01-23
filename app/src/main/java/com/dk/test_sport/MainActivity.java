f4de3b71451e1e967ce19c6d5d90cc98ab5c28e5
Was404 <vladimirovichdanil@bk.ru>
2023-01-23 22:08:06 +0300

    webview, InternetDetector, dialog, remote_config

--- app/src/main/java/com/dk/test_sport/MainActivity.java
+++ app/src/main/java/com/dk/test_sport/MainActivity.java
@@ -1,110 +1,126 @@
 package com.dk.test_sport;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity  {
 
-    TextView questions;
-    String[] questionArray;
-    String[] variable1;
-    String[] variable2;
-    String[] variable3;
+    private WebView webview;
 
-    ImageButton btn_next;
-    Button btn_1;
-    Button btn_2;
-    Button btn_3;
-    int count = 1;
-    int btn_var1 = 0;
-    int k = 0;
+    InternetDetector cd;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        questions = findViewById(R.id.questions);
-        questionArray = getResources().getStringArray(R.array.questionArray);
-        variable1 = getResources().getStringArray(R.array.variable1);
-        variable2 = getResources().getStringArray(R.array.variable2);
-        variable3 = getResources().getStringArray(R.array.variable3);
-        btn_next = findViewById(R.id.btn_next);
-        btn_1 = findViewById(R.id.btn_1);
-        btn_2 = findViewById(R.id.btn_2);
-        btn_3 = findViewById(R.id.btn_3);
+        //ВЕБВЬЮ
+        webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("https://firebase.google.com/docs/remote-config/get-started?platform=android#firebase-console");
+        webview.setWebViewClient(new WebViewClient());
+        webview.setVisibility(View.INVISIBLE);
+        //КОНЕЦ ВЕБВЬЮ
 
-        questions.setText(questionArray[0]);
-        btn_1.setText(variable1[0]);
-        btn_2.setText(variable2[0]);
-        btn_3.setText(variable3[0]);
+        //ИНТЕРНЕТ
+        cd = new InternetDetector(this);
+        //
 
-        btn_next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (count <= 7) {
-                    questions.setText(questionArray[count]);
-                    btn_1.setText(variable1[count]);
-                    btn_2.setText(variable2[count]);
-                    btn_3.setText(variable3[count]);
-                    count ++;
-                    btn_1.setVisibility(View.VISIBLE);
-                    btn_2.setVisibility(View.VISIBLE);
-                    btn_3.setVisibility(View.VISIBLE);
-                    k ++;
-                }
-                else {
-                    if (btn_var1 >= 7) {
-                        questions.setText(R.string.test_success);
+        //ЗДЕСЬ ПРОВЕРКА СОХРАНЕНА ЛИ ССЫЛКА(If else)
+        //Ссылка не сохранена
+        FirebaseRemoteConfig mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(3600)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+        mFirebaseRemoteConfig.setDefaultsAsync(R.xml.remote_config_defaults);
+        mFirebaseRemoteConfig.fetchAndActivate()
+                .addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Boolean> task) {
+                        if (task.isSuccessful()) {
+                            boolean updated = task.getResult();
+                            Log.d(TAG, "Config params updated: " + updated);
+                            Toast.makeText(MainActivity.this, "Fetch and activate succeeded",
+                                    Toast.LENGTH_SHORT).show();
+                            String find = mFirebaseRemoteConfig.getString("url");
+                            if (find == "EMULATOR||NO SIM"){
+                                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                                startActivity(intent);
+                            }else {
+                                //Иначе сохранить ссылку и запуск вебвью
+                            }
+                            //Ссылка сохранена
+                            //Интернет отключён?
+                            if (!cd.isConnected()){
+                                Toast.makeText(MainActivity.this, "Please turn internet on", Toast.LENGTH_SHORT).show();
+                                showDialog("com.dk.test_sport");
+                            } else {
+                                webview.setVisibility(View.VISIBLE);
+                            }
+
+                        } else {
+                            Toast.makeText(MainActivity.this, "Fetch failed",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                            startActivity(intent);
+                        }
+
+                        // displayWelcomeMessage();
                     }
-                    else if (btn_var1 < 7 && btn_var1 > 2){
-                        questions.setText(R.string.test_merge);
-                    }
-                    else if (btn_var1 <= 2){
-                        questions.setText(R.string.test_failed);
-                    }
-                    k = -1;
-                    count = 0;
-                    btn_var1 = 0;
-                    btn_1.setVisibility(View.INVISIBLE);
-                    btn_2.setVisibility(View.INVISIBLE);
-                    btn_3.setVisibility(View.INVISIBLE);
-                }
-            }
-        });
-        btn_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (k == 0 || k == 4){
-                    btn_var1 += 1;
-                }
-                btn_2.setVisibility(View.INVISIBLE);
-                btn_3.setVisibility(View.INVISIBLE);
-            }
-        });
-        btn_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (k == 3 || k == 5){
-                    btn_var1 += 1;
-                }
-                btn_1.setVisibility(View.INVISIBLE);
-                btn_3.setVisibility(View.INVISIBLE);
-            }
-        });
-        btn_3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (k == 1 || k == 2 || k == 6 || k == 7){
-                    btn_var1 += 1;
-                }
-                btn_1.setVisibility(View.INVISIBLE);
-                btn_2.setVisibility(View.INVISIBLE);
-            }
-        });
+                });
+
     }
+        public class WebViewClient extends android.webkit.WebViewClient {
+            @TargetApi(Build.VERSION_CODES.N)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+            //для старых устройств
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        }
+        //ВКЛЮЧИ ИНЕТ
+        private void showDialog(final String appPackageName){
+            final AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("Warning")
+                    .setMessage("The application requires an internet connection")
+                    .setPositiveButton("Ok", null)
+                    .show();
+        }
+        //
+
 }
+
+
